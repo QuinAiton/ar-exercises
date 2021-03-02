e0ee6d7261583a72097367b6cd4aca60c0716367
QuintenAiton <quinaiton@hotmail.com>
2021-03-02 21:05:37 +0000

    gets store by ID and Updates Name

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -13,8 +13,8 @@ puts "----------"
 #   end
 # end
 
-store1 = Store.create(name:'Burnaby', annual_revenue:300000, mens_apparel:true, womens_apparel:true)
-store2 = Store.create(name:'Richmond', annual_revenue:1260000, womens_apparel:true)
-store3 = Store.create(name:'Gastown', annual_revenue:190000, mens_apparel:true,)
+Store.create(name:'Burnaby', annual_revenue:300000, mens_apparel:true, womens_apparel:true)
+Store.create(name:'Richmond', annual_revenue:1260000, womens_apparel:true)
+Store.create(name:'Gastown', annual_revenue:190000, mens_apparel:true,)
 
 puts Store.count
